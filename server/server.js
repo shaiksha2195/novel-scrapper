7994a80f5fadbcd051b4ca69ef74793c59525a25
Shaik <mr.shaiksha@gmail.com>
2019-06-25 07:17:35 +0530

    test for python node integration/ cross-communication

--- server/server.js
+++ server/server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-    const filepath = 'python/test.py';
+    const filepath = 'python';
     const spawn = require("child_process").spawn;
     const pyprocess = spawn('python3',[filepath, '-l']);
 
