fc6f451c9ea7c1c1d0543326195896583f771ca7
Shaik <mr.shaiksha@gmail.com>
2019-06-27 11:17:01 +0530

    recoded everything so that python script saves to json and node reads json and stores in sql

--- server/python/scrapper.py
+++ server/python/scrapper.py
@@ -2,37 +2,79 @@ from bs4 import BeautifulSoup as bs, SoupStrainer
 import requests
 import sys
 import json
+import os
 
-page = requests.get('http://www.boxnovel.com/novel/' + sys.argv[1])
+
+# In[130]:
+
+
+page = requests.get('https://boxnovel.com/novel/'+sys.argv[1])
 soup = bs(page.content, 'html.parser')
+
+
+# In[131]:
+
+
+lastc = []
+if (os.path.exists('novels/'+sys.argv[1]+'.json')):
+    with open('novels/'+sys.argv[1]+'.json', 'r', encoding='utf-8') as f:
+        chap_dict = json.load(f)
+    for distro in chap_dict:
+        lastc.append(distro)
+
+
+# In[132]:
+
 
 ulit = soup.find_all('li', class_='wp-manga-chapter')
 arr = []
 
+
+# In[133]:
+
+
 for x in ulit:
     arr.extend([y.get_attribute_list('href') for y in x.find_all('a')][0])
+
+
+# In[134]:
+
 
 arr.reverse()
 toupdate = []
 for x in arr:
     toupdate.extend([int(s) for s in x.split('-') if s.isdigit()])
-x = toupdate.index(int(sys.argv[2]) + 1)
+
+
+# In[135]:
+
 
 tot = []
 titles = []
-
-for c in arr[x:]:
+for c in arr:
     chap = requests.get(c)
     chapsoup = bs(chap.content, 'html.parser')
     chacont = chapsoup.div.select('.cha-words p')
     chatitle = chapsoup.div.select('.cha-tit h3')
+    if chatitle[0].get_text() in lastc:
+        continue
     tot.append(' '.join([str(i) for i in chacont]))
     titles.append(chatitle[0].get_text())
 
+
+# In[136]:
+
+
 final = dict(zip(titles,tot))
+final.update(chap_dict)
 
-exportfile = json.dumps({"chapters":final, "last-chap":toupdate[-1]})
 
-print(exportfile)
+# In[138]:
+
+
+with open('novels/'+sys.argv[1]+'.json', 'w+', encoding='utf-8') as fp:
+    json.dump(final, fp, indent=2, ensure_ascii=False)
+
+print("completed!")
 
 sys.stdout.flush()
--- server/server.js
+++ server/server.js
@@ -11,37 +11,21 @@ var db = require('knex')({
     }
   });
 
-let novel = ['zombie-sister-strategy', 'i-alone-level-up'];
-let last;
 
-db.select('*').from('novels').where({novel:novel[0]}).then((data) => {
-    if(!data[0]){
-        db('novels').insert({novel:novel[0], lastchap:0});
-        last = 0;
-    }
-    else{
-        last = data[0].lastchap;
-    }
-    const result = require('./zombie-sister-strategy.json');
-
-    let chaps = '';
-    for(let titles in result){
-        chaps = result[titles];
-        db.raw('insert into chapters (novel, num, title, content) values(?, ?, ?, ?) on conflict do nothing', [novel[0], 1, titles, chaps])
-        .then(console.log(titles));
-    }
-});
-
-app.get('/novel/:link/:chap', (req, res) => {
+app.get('/novel/:link', (req, res) => {
+    
     const filepath = 'python/scrapper.py';
     const spawn = require("child_process").spawn;
-    const pyprocess = spawn('python',[filepath, req.params.link, req.params.chap, '-l']);
-
+    const pyprocess = spawn('python',[filepath, 'mr-fu-i-really-love-you', '15', '-l']);
+    
+    let fin;
+    
     pyprocess.stdout.on('data', (data) => {
-        const text = data.toString('utf-8');
-        const fin = JSON.parse(text)
-        res.json(fin);
-    })
+    const text = data.toString('utf-8');
+    fin = JSON.parse(text);
+    res.send(fin);
+    });
 });
- 
+   
+
 app.listen(3000);
