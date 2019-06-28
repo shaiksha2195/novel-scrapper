701fcfc14cf514db6c370211e47cde3a9a2f563a
Shaik <mr.shaiksha@gmail.com>
2019-06-28 18:40:08 +0530

    Storing all the content in json is successful and it is also possible to retrieve individual chapters and all novels list

--- server/python/scrapper.py
+++ server/python/scrapper.py
@@ -3,7 +3,7 @@ import requests
 import sys
 import json
 import os
-
+import re
 
 # In[130]:
 
@@ -16,65 +16,90 @@ soup = bs(page.content, 'html.parser')
 
 
 lastc = []
-if (os.path.exists('novels/'+sys.argv[1]+'.json')):
-    with open('novels/'+sys.argv[1]+'.json', 'r', encoding='utf-8') as f:
+chap_dict = dict()
+if (os.path.exists('../novels/'+sys.argv[1]+'.json')):
+    with open('../novels/'+sys.argv[1]+'.json', 'r', encoding='utf-8') as f:
         chap_dict = json.load(f)
     for distro in chap_dict:
         lastc.append(distro)
-
-
+else:
+    lastc.append(' ')
 # In[132]:
-
-
 ulit = soup.find_all('li', class_='wp-manga-chapter')
 arr = []
 
-
 # In[133]:
-
-
+lastc.reverse()
+xd = lastc[0]
+stop = 0
 for x in ulit:
-    arr.extend([y.get_attribute_list('href') for y in x.find_all('a')][0])
-
+    if stop == 1:
+        break
+    for y in x.find_all('a'):
+        h = y.get_text()
+        if (re.search(h.strip(), xd.replace(':', ' -'))):
+            stop = 1
+        arr.extend([y.get_attribute_list('href') for y in x.find_all('a')][0])
 
 # In[134]:
-
 
 arr.reverse()
 toupdate = []
 for x in arr:
     toupdate.extend([int(s) for s in x.split('-') if s.isdigit()])
 
+f = open('../novels/data.json','w+', encoding='utf-8')
+json.dump({sys.argv[1] : ' '}, f, indent=4)
+
+filename = '../novels/data.json'
+with open(filename, 'r', encoding='utf-8') as f:
+    data = json.load(f)
+    data[sys.argv[1]] = "Adding Chapters"
+
+os.remove(filename)
+with open(filename, 'w', encoding='utf-8') as f:
+    json.dump(data, f, indent=4)
 
 # In[135]:
 
-
 tot = []
 titles = []
+count = 0
 for c in arr:
     chap = requests.get(c)
     chapsoup = bs(chap.content, 'html.parser')
-    chacont = chapsoup.div.select('.cha-words p')
-    chatitle = chapsoup.div.select('.cha-tit h3')
+    chacont = chapsoup.div.select('.cha-words p') if chapsoup.div.select('.cha-words p') != [] else chapsoup.div.select('.reading-content p')
+    chatitle = chapsoup.div.select('.cha-tit h3') if chapsoup.div.select('.cha-words p') != [] else chapsoup.div.select('.reading-content h2') if chapsoup.div.select('.reading-content h2') != [] else chapsoup.find_all('option', selected=True)
     if chatitle[0].get_text() in lastc:
         continue
     tot.append(' '.join([str(i) for i in chacont]))
     titles.append(chatitle[0].get_text())
+    count += 1
+    if count == 10:
+        count = 0
+        temp_dict = (zip(titles, tot))
+        chap_dict.update(temp_dict)
+        with open('../novels/'+sys.argv[1]+'.json', 'w+', encoding='utf-8') as fp:    
+            json.dump(chap_dict, fp, indent=2, ensure_ascii=False)
+
+final = dict(zip(titles, tot))
+chap_dict.update(final)
+
+with open('../novels/'+sys.argv[1]+'.json', 'w+', encoding='utf-8') as fp:    
+    json.dump(chap_dict, fp, indent=2, ensure_ascii=False)
 
 
-# In[136]:
+filename = '../novels/data.json'
+with open(filename, 'r', encoding='utf-8') as f:
+    data = json.load(f)
+    data[sys.argv[1]] = "Updated"
 
-
-final = dict(zip(titles,tot))
-final.update(chap_dict)
-
-
-# In[138]:
-
-
-with open('novels/'+sys.argv[1]+'.json', 'w+', encoding='utf-8') as fp:
-    json.dump(final, fp, indent=2, ensure_ascii=False)
+os.remove(filename)
+with open(filename, 'w', encoding='utf-8') as f:
+    json.dump(data, f, indent=4)
 
 print("completed!")
 
 sys.stdout.flush()
+
+#%%
--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const app = express();
+const fs = require('fs');
 
 var db = require('knex')({
     client: 'pg',
@@ -16,16 +17,36 @@ app.get('/novel/:link', (req, res) => {
     
     const filepath = 'python/scrapper.py';
     const spawn = require("child_process").spawn;
-    const pyprocess = spawn('python',[filepath, 'mr-fu-i-really-love-you', '15', '-l']);
-    
-    let fin;
+    const pyprocess = spawn('python',[filepath, req.params.link.toString()]);
     
     pyprocess.stdout.on('data', (data) => {
     const text = data.toString('utf-8');
-    fin = JSON.parse(text);
-    res.send(fin);
+    console.log(text);
     });
+    
+    res.send("Adding the Novel!");
 });
-   
+  
+app.get('/get/:link/:chap', (req, res) => {
+  let jsonfile = require('./python/novels/' + req.params.link.toString() +'.json');
+  let num = parseInt(req.params.chap) - 1;
+  try{
+    let x = '<h2 class = "title">' + Object.keys(jsonfile)[num] + '</h2>'
+            + Object.values(jsonfile)[num];
+  
+    res.send(x);
+  }
+  catch{
+    res.send("no such novel or chapter!")
+  }
+});
+
+app.get('/getlist', (req, res) => {
+  const folder = '../novels';
+
+  fs.readdir(folder, (err, files) => {
+    res.json({"files" : files});
+  });
+});
 
 app.listen(3000);
