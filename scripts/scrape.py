16bbf83d6f02086c8c65649401a055eb6a210121
shaik sha <mr.shaiksha@gmail.com>
2019-08-20 15:54:57 +0530

    server returns html with novel instead of json

--- scripts/scrape.py
+++
@@ -1,6 +0,0 @@
-from bs4 import BeautifulSoup as bs;
-
-with open("index.html") as fp:
-    soup = bs(fp)
-
-soup.bs("chapter")
--- server/server.js
+++ server/server.js
@@ -28,20 +28,42 @@ app.get('/add/:link', (req, res) => {
 });
   
 app.get('/:link/:chap', (req, res) => {
-  let jsonfile = require('./python/novels/' + req.params.link.toString() +'.json');
+  let jsonfile = require('../novels/' + req.params.link.toString() +'.json');
   let num = parseInt(req.params.chap) - 1;
   try{
     let x = '<h2 class = "title">' + Object.keys(jsonfile)[num] + '</h2>'
             + Object.values(jsonfile)[num];
-  
-    res.send(x);
+    let sel;
+    let i = 1;
+    let j = num +1;
+    Object.keys(jsonfile).forEach(element => {
+      if(j  === i)
+      {
+        sel += "<option value = '"+ i +"' selected>"+ element+1 + "</option>";
+      }
+      else{
+        sel += "<option value = '"+ i +"'>"+ element+1 + "</option>";
+      }
+      i++;
+    });
+
+    let options = "<button><a href='" + num++ + "'>Prev</a></button><select  onchange='location = this.value;'>" + sel + "</select><button><a href='"+ ++num + "'>Next</a></button><button><a href='http://localhost:3000'>HOME</a></button>";
+    let result = options + x + options;
+
+    if( Object.keys(jsonfile)[num] === undefined)
+    {
+      res.send(options + "<br>no such novel or chapter!")
+    }
+    else{
+      res.send("<div style='padding-bottom:100px;'>" + result + "</div>");
+    }
   }
   catch{
     res.send("no such novel or chapter!")
   }
 });
 
-app.get('/getlist', (req, res) => {
+app.get('/', (req, res) => {
   const folder = '../novels';
   let list = '';
   let i = 0;
@@ -50,10 +72,11 @@ app.get('/getlist', (req, res) => {
       if (i == 0)
         i++;
       else
-        list += '<li><a href="/novels/' + el.split('.')[0] + '" style="text-decoration:none;color:black">' + el.split('.')[0] + '</a></li>';
+        list += '<li><a href="/' + el.split('.')[0] + '/1" style="text-decoration:none;color:black">' + el.split('.')[0] + '</a></li>';
     });
-    res.send('<ul style="list-style:none">' + list + '</ul>');
+    res.send('<h2>Novels</h2><ul style="list-style:none">' + list + '</ul>');
   });
 });
+
 
 app.listen(3000);
